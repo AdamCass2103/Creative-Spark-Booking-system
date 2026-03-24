0a24de79d6f75890e11adea565f8e4bc5d230977
Adam Cassidy <D00265095@student.dkit.ie>
2026-03-24 12:48:34 +0000

    test

--- member/payment.php
+++ member/payment.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../includes/config.php';
 require_once __DIR__ . '/../includes/functions.php';     
 require_once __DIR__ . '/../includes/auth.php';
 requireLogin();
-
+//222
 // Get database connection
 $conn = getDatabaseConnection();
 if (!$conn) {
